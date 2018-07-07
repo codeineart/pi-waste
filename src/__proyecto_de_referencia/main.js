ca0c3b0357c2c322ca9702999b3781067efd6869
Ian Franco Sebástian <ian.pozo.m@gmail.com>
2018-07-07 15:18:22 -0400

    Limpieza de proyecto

--- src/__proyecto_de_referencia/main.js
+++ src/__proyecto_de_referencia/main.js
@@ -1,53 +1,56 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import Vuex from 'vuex';
-import Router from 'vue-router';
-import VueSession from 'vue-session';
-import VueCookie from 'vue-cookie';
-import './directives/show-adnimation';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Router from 'vue-router'
+import VueSession from 'vue-session'
+import VueCookie from 'vue-cookie'
+import './directives/show-adnimation'
 
 // Visual imports
-import icons from 'glyphicons';
+import icons from 'glyphicons'
 
 // Vue importing
-Vue.use(VueCookie);
-Vue.use(Router);
-Vue.use(Vuex);
+Vue.use(VueSession, {
+  persist: true
+})
+Vue.use(VueCookie)
+Vue.use(Router)
+Vue.use(Vuex)
 Vue.mixin({
   methods: {
     // Gets rid of reactive stuff when printing data objects
     purify: reactiveObject => {
-      return JSON.parse(JSON.stringify(reactiveObject));
-    },
-  },
-});
+      return JSON.parse(JSON.stringify(reactiveObject))
+    }
+  }
+})
 
-// Fo logging ease of use
 Vue.prototype.$cout = (content = '{{ Input Variable }}', title = 'Simple logging helper:', clear = false, banner = false, purify = false) => {
   if (clear == true) {
-    console.clear();
+    console.clear()
   }
   if (banner == true) {
-    console.error(`###################### ${title} ##########################`);
+    console.error(`###################### ${title} ##########################`)
   }
 
-  let output = purify ? JSON.parse(JSON.stringify(content)) : content;
+  let output = purify ? JSON.parse(JSON.stringify(content)) : content
 
   if (typeof output === 'Array') {
-    console.log('Is Array');
+    console.log('Is Array')
   }
 
-  console.info('cout >> ' + title);
-  console.log(output);
-};
+  console.info('cout >> ' + title)
+  console.log(output)
+}
 
-import Datatable from 'vue2-datatable-component';
-Vue.use(Datatable);
+import Datatable from 'vue2-datatable-component'
+Vue.use(Datatable)
 
 // Router components
-import App from 'src/App';
-import router from 'src/router';
+import App from 'src/App'
+import router from 'src/router'
+import store from 'src/store'
 
 /**
  * to: Route: the target Route Object being navigated to.
@@ -60,27 +63,28 @@ router.beforeEach((to, from, next) => {
     // navigation requires auth, dispatch
     store.dispatch('AUTH_TOKEN_CHECK').then(tokenValid => {
       if (!tokenValid) {
-        alert('Acceso denegado');
-        next('/login');
+        alert('Acceso denegado')
+        next('/login')
       } else {
         // continue navigation
-        next();
+        next()
       }
-    });
+    })
   } else {
     // route without authorization required
-    next();
+    next()
   }
-});
+})
 
-Vue.config.productionTip = false;
-Vue.config.silent = false;
+Vue.config.productionTip = false
+Vue.config.silent = false
 
 const app = new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: {
-    App,
-  },
-});
+    App
+  }
+})
